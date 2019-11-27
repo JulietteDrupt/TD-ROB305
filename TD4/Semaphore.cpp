b88fef46a89f8d4dafa4416efdde482934cc7f36
Juliette DRUPT <juliette.drupt@etu.emse.fr>
2019-11-27 17:54:54 +0100

    Completed Semaphore class, not tested yet

--- TD4/Semaphore.cpp
+++ TD4/Semaphore.cpp
@@ -1,13 +1,13 @@
 #include "Semaphore.h"
+#include "Mutex.h"
 #include <pthread.h>
-#include <climits>
 
 Semaphore::Semaphore(unsigned int initCount, unsigned int maxCount) : counter(initCount), maxCount(maxCount)
 {}
 
 void give()
 {
-	if (counter < UINT_MAX)
+	if (counter < maxCount)
 	{
 		this -> counter += 1;
 	}
@@ -18,6 +18,31 @@ void take()
 	if (counter > 0)
 	{
 		this -> counter -= 1;
+	}
+
+	if (counter == 0)
+	{
+		// si le compteur de jetons est à zéro, appel bloquant
+		Mutex::Lock ml = Mutex::Lock(this -> mutex);
+		ml.wait();
+		ml.~Lock();
+	}
+}
+
+bool take(double timeout_ms)
+{
+	if (counter > 0)
+	{
+		this -> counter -= 1;
+	}
+
+	if (counter == 0)
+	{
+		// si le compteur de jetons est à zéro, appel bloquant
+		Mutex::Lock ml = Mutex::Lock(this -> mutex);
+		bool val = ml.wait(timeout_ms);
+		ml.~Lock();
+		return val;
 	}
 }
 
