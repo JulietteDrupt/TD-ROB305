2f5f312875ea77a5af67b52e3f862c19884607e9
Juliette DRUPT <juliette.drupt@etu.emse.fr>
2019-11-28 17:55:26 +0100

    Finished TD4c (ie finished TD4)!

--- TD4/td4d-main.cpp
+++ TD4/td4d-main.cpp
@@ -6,7 +6,28 @@ int main()
 {
 	Fifo<int> fifo;
 	int n = 5;
-	FifoProducerThread fpt = FifoProducerThread(fifo,n);
-	FifoConsumerThread fct = FifoConsumerThread(fifo,n);
+
+	FifoProducerThread *prodThreads[n];
+	FifoConsumerThread *consThreads[n];
+
+	for (int i=0; i<n; i++)
+	{
+		prodThreads[i] = new FifoProducerThread(fifo,n);
+		consThreads[i] = new FifoConsumerThread(fifo,n);
+	}
+
+	for (int i=0; i<n; i++)
+	{
+		prodThreads[i] -> start();
+		consThreads[i] -> start();
+	}
+
+	for (int i=0; i<n; i++)
+	{
+		prodThreads[i] -> join();
+		consThreads[i] -> join();
+	}
+
 	return 0;
 }
+
