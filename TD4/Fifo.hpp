2d652b1ebedf40a26ecb2dab64847c949d364596
Juliette DRUPT <juliette.drupt@etu.emse.fr>
2019-11-28 15:43:35 +0100

    Created Fifo class, not tested yet

---
+++ TD4/Fifo.hpp
@@ -0,0 +1,73 @@
+#include "Mutex.h"
+
+template <typename T>
+
+class Fifo
+{
+	public :
+		class EmptyException;
+		void push(T element);
+		T pop();
+		T pop(double timeout_ms) : throw EmptyException;
+
+	protected :
+		Mutex m;
+
+	private :
+		std::queue<T> elements;
+};
+
+class Fifo::EmptyException
+{
+	virtual const char* what() const throw()
+	{
+		return "Empty exception";
+	}
+};
+
+void Fifo::push(T element)
+{
+	if ((this -> elements).empty())
+	{
+		Mutex::TryLock mtl = Mutex::TryLock(this -> mutex);
+		mtl.notify();
+		mtl.~TryLock();
+	}
+	(this -> elements).push(T);
+}
+
+T Fifo::pop()
+{
+	T val;
+	if ((this -> elements).empty())
+	{
+		Mutex::Lock ml = Mutex::Lock(this -> mutex);
+		ml.wait();
+		ml.~Lock();
+	}
+	else
+	{
+		val = (this -> elements).front();
+		(this -> elements).pop();
+	}
+	return val;
+}
+
+T Fifo::pop(double timeout_ms)
+{
+	T val;
+	if ((this -> elements).empty())
+	{
+		throw Fifo:EmptyException();
+		Mutex::Lock ml = Mutex::Lock(this -> mutex, timeout_ms);
+		ml.wait();
+		ml.~Lock();
+	}
+	else
+	{
+		val = (this -> elements).front();
+		(this -> elements).pop();
+	}
+	return val;
+}
+
